34fb922084f5dce31c302389aba7b61dfe35989e
tamliad123456 <tamliad123456@gmail.com>
2017-08-18 14:21:55 +0300

    bug fixes #omri check the program

--- server.py
+++ server.py
@@ -2,11 +2,24 @@ import socket
 from threading import Thread
 from hashlib import md5	
 import sys
+import json
 
 LISTENING_PORT = 4325
 unamePass = {}
 
+def read_users():
+	'''The funcion reading the usernames and the passwords from the users.dat file.'''
+	global unamePass
+	try:
+		file = open('users.dat', 'r')
+		data = file.read()
+		unamePass = json.loads(data)
+		file.close()
+	except Exception as e:
+		print(e)
+
 def main():
+	read_users()
 	listening_socket = socket.socket()
 	listening_socket.bind(('0.0.0.0', LISTENING_PORT))
 	listening_socket.listen(10)
@@ -19,16 +32,6 @@ def send_data(string_to_send,socket):
 	'''The function sending the string to the client'''
 	socket.send(bytes(string_to_send,"utf-8"))
 
-def read_users():
-	'''The funcion reading the usernames and the passwords from the users.dat file.'''
-	global unamePass
-	try:
-		file = open('users.dat', 'r')
-		data = file.read()
-		unamePass = json.loads(data)
-		file.close()
-	except Exception as e:
-		print(e)
 
 def hashing_creator_md5(string):
 	'''The function is getting string and returning md5 hash'''
@@ -64,7 +67,7 @@ def proccess_socket(partner_socket):
 			if temp[2] == '1':  #the user want to register
 				send_data('Username already exist.', partner_socket)
 			else:
-				if temp[1] == unamePass[temp[0]]:  #checking if the password is equals to the password that sent.
+				if hashing_creator_md5(temp[1]) == unamePass[temp[0]]:  #checking if the password is equals to the password that sent.
 					send_data('Connected succefully!', partner_socket)
 				else:  #the password is wrong.
 					send_data('Password and username does not match.', partner_socket)
